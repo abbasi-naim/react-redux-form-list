6892f7f3e78e92da07432d31a8e929c57c20a086
abbasi-naim <abbasi.naim@gmail.com>
2022-02-06 14:11:39 +0300

    align buttons to the center

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import "./App.css";
 import AppHeader from "./Components/Header";
 import ProjectDialogForm from "./Components/ProjectDialogForm";
 import UsersDialogForm from "./Components/UsersDialogForm";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Typography from "@mui/material/Typography";
 
 function App() {
   return (
     <div>
       <AppHeader />
-      <span>
-        <ProjectDialogForm />
-        <UsersDialogForm />
-      </span>
+      <Typography align="center">
+        <ButtonGroup
+          size="small"
+          aria-label="small button group"
+          sx={{ mt: 1, ml: 5 }}
+        >
+          <ProjectDialogForm />
+          <UsersDialogForm />
+        </ButtonGroup>
+      </Typography>
     </div>
   );
 }
